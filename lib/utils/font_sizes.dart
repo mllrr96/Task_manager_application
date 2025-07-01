1dc8584d9fa40d24b772e31f9fdbcd929f2a8db1
Mo <mrmoragdr@gmail.com>
2025-07-01 21:23:39 +0300

    Delete unused files

--- lib/utils/font_sizes.dart
+++
@@ -1,6 +0,0 @@
-// const double textTiny = 10.0; // Theme.of(context).textTheme.labelSmall
-// const double textSmall = 12.0; // Theme.of(context).textTheme.labelMedium
-// const double textMedium = 14.0; // Theme.of(context).textTheme.bodySmall
-// const double textExtraLarge = 18.0; // Theme.of(context).textTheme.titleMedium
-// const double textXExtraLarge = 20.0; // Theme.of(context).textTheme.titleLarge
-// const double textBold = 30.0; // Theme.of(context).textTheme.headlineLarge
